b9dd5237562c19c34e4aea180508a4bc14affdb1
Ishana0 <ishanakalikiri01@gmailcom>
2024-02-09 11:28:41 +0530

    router

---
+++ router-lab/src/App.jsx
@@ -0,0 +1,15 @@
+import AllRoutes from './Routes/AllRoutes'
+import Navbar from './Components/Navbar'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <AllRoutes />
+    </>
+  )
+}
+
+export default App;
